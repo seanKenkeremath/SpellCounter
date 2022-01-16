f1f73374eefe0b8986dfd989454c37d338cf6ec9
Sean Kenkeremath <sean.kenkeremath@willowtreeapps.com>
2022-01-16 16:24:33 -0500

    added game menu button

--- app/src/main/java/com/kenkeremath/mtgcounter/ui/MainActivity.kt
+++ app/src/main/java/com/kenkeremath/mtgcounter/ui/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
+        setContentView(R.layout.activity_main)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, SetupFragment.newInstance())
--- app/src/main/java/com/kenkeremath/mtgcounter/ui/game/GameActivity.kt
+++ app/src/main/java/com/kenkeremath/mtgcounter/ui/game/GameActivity.kt
@@ -2,9 +2,14 @@ package com.kenkeremath.mtgcounter.ui.game
 
 import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.drawable.RippleDrawable
 import android.os.Bundle
 import android.view.View
+import android.view.ViewTreeObserver
 import android.view.WindowManager
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
@@ -18,9 +23,12 @@ import com.kenkeremath.mtgcounter.model.TabletopType
 import com.kenkeremath.mtgcounter.model.player.PlayerSetupModel
 import com.kenkeremath.mtgcounter.ui.game.rv.GamePlayerRecyclerAdapter
 import com.kenkeremath.mtgcounter.ui.game.tabletop.GameTabletopLayoutAdapter
+import com.kenkeremath.mtgcounter.view.TableLayoutPosition
 import com.kenkeremath.mtgcounter.view.TabletopLayout
 import com.kenkeremath.mtgcounter.view.counter.edit.PlayerMenuListener
 import dagger.hilt.android.AndroidEntryPoint
+import android.util.TypedValue
+
 
 @AndroidEntryPoint
 class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
@@ -34,6 +42,8 @@ class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
             )
         }
     }
+
+    private lateinit var gameContainer: FrameLayout
 
     private lateinit var tabletopContainer: RotateLayout
     private lateinit var tabletopLayout: TabletopLayout
@@ -49,6 +59,8 @@ class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
         setContentView(R.layout.activity_game)
 
         hideSystemUI()
+
+        gameContainer = findViewById(R.id.game_container)
 
         tabletopContainer = findViewById(R.id.tabletop_container)
         tabletopLayout = findViewById(R.id.tabletop_layout)
@@ -81,6 +93,8 @@ class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
             tabletopContainer.visibility = View.VISIBLE
         }
 
+        addMenuButton()
+
         viewModel.players.observe(this) {
             tabletopLayoutAdapter.updateAll(viewModel.tabletopType, it)
             playersRecyclerAdapter.setData(it)
@@ -93,6 +107,151 @@ class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
                 window.clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
             }
         }
+    }
+
+    private fun addMenuButton() {
+        tabletopLayout.viewTreeObserver.addOnPreDrawListener(object :
+            ViewTreeObserver.OnPreDrawListener {
+            override fun onPreDraw(): Boolean {
+                tabletopLayout.viewTreeObserver.removeOnPreDrawListener(this)
+                val container = FrameLayout(this@GameActivity)
+                val containerSize =
+                    resources.getDimensionPixelSize(R.dimen.game_menu_button_container_diameter)
+                val containerLp = FrameLayout.LayoutParams(
+                    containerSize,
+                    containerSize,
+                )
+                val menuButton = ImageView(this@GameActivity)
+                menuButton.setImageResource(R.mipmap.ic_launcher)
+                container.addView(
+                    menuButton, FrameLayout.LayoutParams(
+                        FrameLayout.LayoutParams.MATCH_PARENT,
+                        FrameLayout.LayoutParams.MATCH_PARENT
+                    )
+                )
+                val containerPadding =
+                    resources.getDimensionPixelSize(R.dimen.game_menu_button_container_padding)
+                container.setPadding(
+                    containerPadding,
+                    containerPadding,
+                    containerPadding,
+                    containerPadding
+                )
+
+
+                when (viewModel.tabletopType) {
+                    TabletopType.NONE,
+                    TabletopType.LIST,
+                    TabletopType.ONE_VERTICAL -> {
+                        //Top left
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_corner_bg
+                        )
+                    }
+                    TabletopType.ONE_HORIZONTAL -> {
+                        //Top right (appears as top left)
+                        containerLp.leftMargin = tabletopLayout.width - containerSize
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_corner_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.FOUR_ACROSS,
+                    TabletopType.SIX_CIRCLE,
+                    -> {
+                        //Center in screen
+                        containerLp.topMargin = tabletopLayout.height / 2 - containerSize / 2
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.TWO_HORIZONTAL,
+                    TabletopType.FIVE_ACROSS,
+                    TabletopType.THREE_ACROSS -> {
+                        //Center top (appears as center left)
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_side_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.TWO_VERTICAL -> {
+                        //Center left
+                        containerLp.topMargin = tabletopLayout.height / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_side_bg
+                        )
+                    }
+                    TabletopType.FOUR_CIRCLE -> {
+                        //Center offset from topmost (appears leftmost) intersection so no center portion of player is cut off
+                        containerLp.topMargin =
+                            tabletopLayout.panels[TableLayoutPosition.TOP_PANEL]!!.height
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_side_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.SIX_ACROSS -> {
+                        //Center in topmost (appears leftmost) intersection
+                        containerLp.topMargin =
+                            tabletopLayout.panels[TableLayoutPosition.LEFT_PANEL_1]!!.height - containerSize / 2
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.FIVE_CIRCLE -> {
+                        //Center in bottom (appears rightmost) intersection
+                        containerLp.topMargin =
+                            tabletopLayout.height - tabletopLayout.panels[TableLayoutPosition.LEFT_PANEL_1]!!.height - containerSize / 2
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_bg
+                        )
+                        container.rotation = 90f
+                    }
+                    TabletopType.THREE_CIRCLE -> {
+                        //Center in intersection
+                        containerLp.topMargin =
+                            tabletopLayout.panels[TableLayoutPosition.TOP_PANEL]!!.height - containerSize / 2
+                        containerLp.leftMargin = tabletopLayout.width / 2 - containerSize / 2
+                        container.background = ContextCompat.getDrawable(
+                            this@GameActivity,
+                            R.drawable.game_menu_button_container_bg
+                        )
+                        container.rotation = 90f
+                    }
+                }
+                gameContainer.addView(container, containerLp)
+                val outValue = TypedValue()
+                theme.resolveAttribute(android.R.attr.selectableItemBackground, outValue, true)
+                container.foreground = RippleDrawable(
+                    ColorStateList.valueOf(
+                        ContextCompat.getColor(
+                            this@GameActivity,
+                            R.color.accent_blue
+                        )
+                    ), null, container.background
+                )
+                container.setOnClickListener {
+                    //TODO: game menu
+                    openExitPrompt()
+                }
+                return false
+            }
+        })
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -119,6 +278,10 @@ class GameActivity : AppCompatActivity(), OnPlayerUpdatedListener,
     }
 
     override fun onBackPressed() {
+        openExitPrompt()
+    }
+
+    private fun openExitPrompt() {
         val dialog = AlertDialog.Builder(this)
             .setTitle(R.string.exit_game)
             .setIcon(R.mipmap.ic_launcher)
