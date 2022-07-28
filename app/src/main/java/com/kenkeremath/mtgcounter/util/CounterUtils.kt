03e7266b604401627d11b6d38a5c8d9d7880520f
Sean Kenkeremath <sean.kenkeremath@willowtreeapps.com>
2022-07-28 13:45:27 -0400

    holding down counter will always increment by 5

--- app/src/main/java/com/kenkeremath/mtgcounter/util/CounterUtils.kt
+++ app/src/main/java/com/kenkeremath/mtgcounter/util/CounterUtils.kt
@@ -6,7 +6,8 @@ object CounterUtils {
     private const val LARGE_INCREMENT = 5
     private const val SMALL_INCREMENT = 1
     //After this many increments while holding we switch to large increments
-    private const val LARGE_INCREMENT_THRESHOLD = 5
+    //TODO: this is 0 so holding always results in large increments. Update based on feedback
+    private const val LARGE_INCREMENT_THRESHOLD = 0
     fun getAmountChangeForHoldIteration(increments: Int): Int {
         return if (increments <= LARGE_INCREMENT_THRESHOLD) {
             SMALL_INCREMENT
